d73fe5def0c056febd16b8b3502d55363118ddb0
Ali İrfan Doğan <alirfandogan@gmail.com>
2025-03-05 23:03:01 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt install nmap wireshark ettercap-text-only nikto hydra aircrack-ng bettercap hashcat binwalk dirb gobuster ffuf apache2 crunch pgpgpg stegcracker foremost libimage-exiftool-perl hexedit git curl macchanger dnsmasq hostapd -y
+sudo apt install nmap wireshark ettercap-text-only nikto hydra aircrack-ng bettercap hashcat binwalk dirb gobuster ffuf apache2 crunch pgpgpg stegcracker foremost libimage-exiftool-perl hexedit curl macchanger dnsmasq hostapd -y
 sudo snap install john-the-ripper
 sudo snap install metasploit-framework
