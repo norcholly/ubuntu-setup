357254ad9ada3d43cc787ebc5f77026803bf3d87
Ali İrfan Doğan <alirfandogan@gmail.com>
2025-03-05 23:31:00 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
 sudo apt install nmap wireshark ettercap-text-only nikto hydra aircrack-ng bettercap hashcat binwalk dirb gobuster ffuf apache2 crunch pgpgpg stegcracker foremost libimage-exiftool-perl hexedit curl macchanger dnsmasq hostapd -y
 sudo snap install john-the-ripper
 sudo snap install metasploit-framework
